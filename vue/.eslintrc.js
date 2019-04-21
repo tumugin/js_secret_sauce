3b3bdfcd19c024ccb1e0568f86c4139bf90bf592
Kazuki Oishi <oishikazuki@gmail.com>
2019-04-22 03:55:11 +0900

    足りなかったものを追加した

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -2,25 +2,33 @@
 
 module.exports = {
   root: true,
+  // Settings for TypeScript linting
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/strongly-recommended', 
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/base',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'standard',
+    'prettier'
   ],
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'vue',
+    '@typescript-eslint'
   ],
   // add your custom rules here
   rules: {
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
